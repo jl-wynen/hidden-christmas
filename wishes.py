1b3ce1ca24bc72826064e00e2a9a0397823574e6
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2020-12-17 01:26:45 +0100

    Reformat

--- wishes.py
+++ wishes.py
@@ -1,27 +1,26 @@
-{"I wish you"                    *
+{"Wish you"                      *
                                 all
                               ([print(
                            any(map(lambda s
-                        :print ('\u2588' if s 
-                     else' ',end=''),(y //(
-                           17*D**(17*x+ y%
-                       17)) %2 for x in range
-                     (0,106))))*'o')for y in map
-                  (lambda g:int(c,16)*(setattr(__builtins__, 'D', 2), 16)[-1] ** (e+17)*
-               0x198+g,range(16,-1,-1))] for c,e in ((
-                     '14002080156015b01676b603a'
-                  'f866c71b70e19830cc30661c330e198'
-               '30cc20661c318e2e1a185ac16cec0ad6aebea'
-            'b2d5568'+'0'*64+'2aab80038ab8b562bab5ba770d'
-         '2d6e962baae2d558e000355558',67),('2a8'+'5'*15+'6c'
-               'aab6d55b6aadb5565aaaaaaafeaadb556daab'
-            '6d555d5555557faab5555a0002aaad5000000002aac'
-         '0aab5aaffaaad55555556caab6d55b6aadb5565aaaaaaaaaa'
-      'aab55bfaab02aaaaaad5556aaab55559555aa000000028001555600'
-   '00555aa80000000000ab56002b0015800ac005655d5aa8'+'0'*32+'a8000'
-'600030001800955555556aaab5555aaaacaaad500000005555800015555d555feaa'
-                          'aaaabdaab6d55b6'
-                          'aadb556a000000a'
-                          'a957555595555ea'
-                        'abaaacaaad55' ,0)))
-         :-D}
+                        :print ('\u2588' if s
+                     else' ',end=''),(y//(17*D**
+                        (17*x + y%17)) %2 for
+                     x in range(0, 106)))) * 'o')
+                  for y in map(lambda g:int(c, 16)*(
+               setattr(__builtins__,'D',2),16)[-1]**(e+
+                     17)*0x198+g,range(16,-1,-1))]
+                  for c,e in (('14002080156015b01676'
+               'b603af866c71b70e19830cc30661c330e19830c'
+            'c20661c318e2e1a185ac16cec0ad6aebeab2d5568'+'0'
+         *64+'2aab80038ab8b562bab5ba770d2d6e962baae2d558e0003'
+              '55558',67),('2a8'+'5'*15+'6caab6d55b6aadb'
+           '5565aaaaaaafeaadb556daab6d555d5555557faab5555a00'
+        '02aaad5000000002aac0aab5aaffaaad55555556caab6d55b6aadb'
+    '5565aaaaaaaaaaaab55bfaab02aaaaaad5556aaab55559555aa00000002800'
+'155560000555aa80000000000ab56002b0015800ac005655d5aa8'+'0'*32+'a800060'
+                      '0030001800955555556aaab5'
+                      '555aaaacaaad500000005555'
+                      '800015555d555feaaaaaabda'
+                      'ab6d55b6aadb556a000000aa'
+                  '957555595555eaabaaacaaad55' ,0)))
+:-D                                                                 }
